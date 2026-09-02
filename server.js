95204d8806d5d42e89f3dd3d2b4d59b9207b87e8
sahil khan <23cssahil@gmail.com>
2026-09-03 00:48:48 +0530

    Fix express rate limit trust proxy and clean up api calls

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const crypto = require('crypto');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', 1);
 app.use(express.json());
 app.set('trust proxy', true);
 app.use(cors());
